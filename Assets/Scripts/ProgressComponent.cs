e081f750715d72c1a2ff787dcd424ce031ee694c
mathis <mathis.chaptinel@epitech.ei>
2020-02-02 01:28:45 +0200

    add robot hp bar

--- Assets/Scripts/ProgressComponent.cs
+++ Assets/Scripts/ProgressComponent.cs
@@ -7,7 +7,6 @@ public class ProgressComponent : MonoBehaviour
 {
     [SerializeField]
     private Transform bar = null;
-    private float filled = 0f;
     
     public void setFill(float fill) {
         bar.localScale = new Vector3(fill, 1f, 1f);
