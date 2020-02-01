556f7efd75c9c93531e963de677182d6ad1d6489
mathis <mathis.chaptinel@epitech.ei>
2020-02-01 10:51:16 +0200

    (feat)factoryComponent: add FactoryComponent and MoneyComponent

---
+++ Assets/Scripts/MoneyComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyComponent : MonoBehaviour
+{  
+    private int money = 0;
+    [SerializeField]
+    private float incomeSpeed = 2f;
+    private float incomeTimer = 2f;
+    [SerializeField]
+    private int income = 1;
+
+    public bool buy(int cost)
+    {
+        if (money < cost)
+            return false;
+        money -= cost;
+        Debug.Log(money);
+        return true;
+    }
+
+    private void Update()
+    {
+        incomeTimer -= Time.deltaTime;
+        if (incomeTimer <= 0f) {
+            money += income;
+            incomeTimer = incomeSpeed;
+            Debug.Log(money);
+        }
+    }
+}
---
+++ Assets/Scripts/ProductionStationComponent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductionStationComponent : SlotComponent
+{
+    [SerializeField]
+    private GameObject toCreate = null;
+    [SerializeField]
+    private int require = 3;
+    [SerializeField]
+    private GameObject owner = null;
+    [SerializeField]
+    private float timeToBuild = 5f;
+    private float timer = 0f;
+    private bool isRunning = false;
+
+    override public PickableComponent TryPick() {
+        PickableComponent tmp = stockedObject;
+
+        if (tmp != null) {
+            stockedObject = null;
+            return tmp;
+        }
+        if (owner.GetComponent<MoneyComponent>().buy(require)) {
+            isRunning = true;
+            timer = 0f;
+        }
+        return null;
+    }
+
+    override public bool TryDrop(PickableComponent toStock) {
+        if (isRunning)
+            return false;
+        return base.TryDrop(toStock);
+    }
+
+    void Update() {
+        if (!isRunning)
+            return;
+        GameObject tmp = null;
+        timer += Time.deltaTime;
+        if (timer >= timeToBuild) {
+            tmp = Instantiate(toCreate, transform.position, transform.rotation);
+            isRunning = false;
+            TryDrop(tmp.GetComponent<PickableComponent>());
+            Debug.Log(stockedObject);
+        }
+    }
+}
