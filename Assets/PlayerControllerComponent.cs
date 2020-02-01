f84ca28b2b7a192a4c5adacd6cc566085accfc58
Clément <clement.baudonnel@epitech.eu>
2020-02-01 02:15:01 +0100

    [Feature] Adding player movement

--- Assets/PlayerControllerComponent.cs
+++ Assets/PlayerControllerComponent.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerControllerComponent : MonoBehaviour
 {
+    [SerializeField]
+    private float speed = 5;
+    private Vector2 movement = Vector2.zero;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        movement = Vector2.up * Input.GetAxisRaw("Vertical") + Vector2.right * Input.GetAxisRaw("Horizontal");
     }
+
+    private void FixedUpdate() {
+        movement *= speed;
+        movement *= Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + movement);
+    }
+
 }
