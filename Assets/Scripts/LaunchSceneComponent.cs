2856f74223ffdf48a457ee6881c8aa4e95180f02
benjaminrenaud3 <benjamin.renaud@epitech.eu>
2020-02-02 12:45:09 +0100

    MoneyComponent with text and MenuComponent

---
+++ Assets/Scripts/LaunchSceneComponent.cs
@@ -0,0 +1,17 @@
+﻿ using UnityEngine;
+ using UnityEngine.SceneManagement;
+ 
+ public class LaunchSceneComponent : MonoBehaviour
+ {
+    [SerializeField]
+    public string scene;
+
+    void Start()
+    {
+    }
+
+    public void LaunchScene()
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+    }
+}
