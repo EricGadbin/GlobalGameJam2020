9aeee0e2279da1fa513b4f1ac67b2b46fda8ab6d
Clément <clement.baudonnel@epitech.eu>
2020-02-08 14:30:54 +0100

    Tried to add controllers, reworked a bit the menus, tried to add particles but it's ugly

---
+++ Assets/ChangeColorComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColorComponent : MonoBehaviour
+{
+    public void ChangeColor(bool RedWon)
+    {
+        GetComponent<Image>().color = RedWon ? Color.red : Color.blue;
+    }
+}
